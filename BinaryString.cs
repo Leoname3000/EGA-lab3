67df08fbc748af4a2a45765b3bcb2f111f6f4d81
Leonid Sulin <sulin.leonid@gmail.com>
2022-10-02 21:43:42 +0300

    Initial commit

---
+++ BinaryString.cs
@@ -0,0 +1,133 @@
+﻿using System;
+namespace EGA_lab3
+{
+    public class BinaryString
+    {
+        public const int MU_FORMAT_WIDTH = 2;                           // Ширина приспособленности при форматированном выводе
+        public const int COUNTER_FORMAT_WIDTH = 2;                      // Ширина счётчика при форматированном выводе
+
+        private const int MOD_B = 2;                                    // Мощность алфавита (2 для бинарного)
+        private int L;                                                  // Длина кодировки
+
+        public const int MU_RAND_MIN = 0, MU_RAND_MAX = 9999;           // Диапазон для случайного вычисления приспособленности
+
+        public string StrVal;                                                       // Значение бинарной строки
+        private static Dictionary<int, int> Mu = new Dictionary<int, int>();        // Словарь, хранящий значения приспособленности
+
+
+        public BinaryString(int length, MuMode muMode)                  // Конструктор, создающий бинарную строку случайным образом
+        {
+            L = length;
+            StrVal = Generate();
+            if (!Mu.ContainsKey(IntVal()))
+                Mu[IntVal()] = MuCalc(muMode);
+        }
+
+        public BinaryString(int length, MuMode muMode, int rawInt)      // Конструктор, создающий бинарную строку для указанного числа
+        {
+            L = length;
+            StrVal = RawIntToStr(rawInt);
+            if (!Mu.ContainsKey(IntVal()))
+                Mu[IntVal()] = MuCalc(muMode);
+        }
+
+        public BinaryString(int length, MuMode muMode, string rawStr)   // Конструктор, создающий бинарную строку добавлением нулей к существующей
+        {
+            L = length;
+            StrVal = FormatRawStr(rawStr);
+            if (!Mu.ContainsKey(IntVal()))
+                Mu[IntVal()] = MuCalc(muMode);
+        }
+
+        private string Generate()                   // Функция генерации случайной бинарной строки
+        {
+            Random random = new Random();
+
+            string binaryString = "";
+            for (int i = 0; i < L; i++)
+            {
+                binaryString = $"{random.Next(0, 10) % MOD_B}" + binaryString;
+            }
+            return binaryString;
+        }
+
+        private string RawIntToStr(int rawInt)      // Функция перевода числа в бинарную строку
+        {
+            string binaryString = "";
+            int remainder;
+
+            while (rawInt > 0)
+            {
+                remainder = rawInt % MOD_B;
+                binaryString = $"{remainder}" + binaryString;
+                rawInt /= MOD_B;
+            }
+            return FormatRawStr(binaryString);
+        }
+
+        private string FormatRawStr(string rawStr)  // Функция форматирования строки (добавляет нули в начале)
+        {
+            string binaryString = rawStr;
+            for (int i = 0; binaryString.Length < L; i++)
+            {
+                binaryString = "0" + binaryString;
+            }
+            return binaryString;
+        }
+
+        public int IntVal()                         // Функция перевода бинарной строки в число
+        {
+            int result = 0;
+
+            for (int i = 0; i < L; i++)
+            {
+                result += Convert.ToInt32($"{StrVal[i]}") * Convert.ToInt32(Math.Pow(MOD_B, L - (i + 1)));
+            }
+            return result;
+        }
+
+        public enum MuMode                          // Режимы вычисления приспособленности:
+        {
+            Rand,                                       // Rand - случайное значение в заданном диапазоне
+            Natural,                                    // Natural - соответствующее натуральное значение
+            Quadratic,                                  // Quadratic - квадратичная функция
+        }
+
+        private int MuCalc (MuMode mode)            // Функция вычисления приспособленности
+        {
+            int mu;
+            switch(mode)
+            {
+                case MuMode.Rand:
+                    Random random = new Random();
+                    mu = random.Next(MU_RAND_MIN, MU_RAND_MAX + 1);
+                    break;
+                case MuMode.Natural:
+                    mu = IntVal();
+                    break;
+                case MuMode.Quadratic:
+                    mu = Convert.ToInt32(Math.Pow(Convert.ToDouble(IntVal()) - Math.Pow(2, L - 1), 2));
+                    break;
+                default:
+                    mu = -1;
+                    break;
+            }
+            return mu;
+        }
+
+        public int GetMu()  // Функция, возвращающая приспособленность для данной бинарной строки
+        {
+            return Mu[IntVal()];
+        }
+
+        public static void CreateLandscape(int stringLength, MuMode muMode, int landscapeLength, int start = 0, int step = 1)  // Функция задания ландшафта приспособленности
+        {
+            for (int i = start; i < start + landscapeLength * step; i += step)
+            {
+                BinaryString landscapeString = new BinaryString(stringLength, muMode, i);
+                Console.WriteLine($"{i,COUNTER_FORMAT_WIDTH} = {landscapeString.StrVal}, mu = {BinaryString.Mu[landscapeString.IntVal()], MU_FORMAT_WIDTH}");
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Text;
+using EGA_lab3;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int L = 5;
+        const BinaryString.MuMode MODE = BinaryString.MuMode.Natural;
+
+        Console.WriteLine("------======= LANDSCAPE =======------\n");
+        BinaryString.CreateLandscape(L, MODE, 32);
+
+        Console.WriteLine("\n------======= HILL CLIMBING METHOD (BREADTH-FIRST SEARCH) =======------\n");
+        Console.WriteLine("Enter N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+        BinaryString maxS = new BinaryString(L, MODE);
+
+        for (int i = 0; i < N; i++)
+        {
+            List<BinaryString> omega = Omega(maxS);
+            string omegaString = $"Omega = {OmegaToString(omega)}\n";
+
+            BinaryString maxOmega = omega[0];
+
+            for (int j = 1; j < omega.Count; j++)
+            {
+                if (maxOmega.GetMu() < omega[j].GetMu())
+                {
+                    maxOmega = omega[j];
+                }
+            }
+
+            string reportString = $"Step {i + 1,BinaryString.COUNTER_FORMAT_WIDTH}: ";
+            reportString += $"maxS = {maxS.StrVal}, max = {maxS.GetMu(),BinaryString.MU_FORMAT_WIDTH}, ";
+            reportString += $"maxOmega = {maxOmega.StrVal}, mu = {maxOmega.GetMu(),BinaryString.MU_FORMAT_WIDTH} ";
+
+            if (maxS.GetMu() < maxOmega.GetMu())
+            {
+                maxS = maxOmega;
+                reportString += "<- CHANGE";
+                Console.WriteLine(reportString);
+                Console.WriteLine(omegaString);
+            }
+            else
+            {
+                Console.WriteLine(reportString);
+                Console.WriteLine(omegaString);
+                break;
+            }
+
+        }
+
+        Console.WriteLine($"Final result: {maxS.StrVal}, mu = {maxS.GetMu()}");
+
+
+        List<BinaryString> Omega(BinaryString currentString)
+        {
+            List<BinaryString> omega = new List<BinaryString>();
+            for (int j = 0; j < L; j++)
+            {
+                StringBuilder nearbyString = new StringBuilder(currentString.StrVal);
+                if (nearbyString[j] == '0')
+                {
+                    nearbyString[j] = '1';
+                }
+                else if (nearbyString[j] == '1')
+                {
+                    nearbyString[j] = '0';
+                }
+                omega.Add(new BinaryString(L, MODE, Convert.ToString(nearbyString)!));
+            }
+            return omega;
+        }
+
+        string OmegaToString(List<BinaryString> omega)
+        {
+            string result = "{ ";
+            if (omega.Count > 0)
+                result += $"{omega[0].StrVal} ({omega[0].GetMu()})";
+            for (int i = 1; i < omega.Count; i++)
+            {
+                result += $", {omega[i].StrVal} ({omega[i].GetMu()})";
+            }
+            result += " }";
+            return result;
+        }
+    }
+}
